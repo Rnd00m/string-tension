14bcd52be080bc8bc5f10e3a52865d7f8536526d
Julien BOILEVIN <j.boilevin@lepermislibre.fr>
2022-08-22 23:41:40 +0200

    refactor: GuitarString

--- src/scripts/classes/GuitarString.ts
+++ src/scripts/classes/GuitarString.ts
@@ -1,15 +1,10 @@
 export class GuitarString {
   constructor(
     public name: string,
-    public size: number, // Size of the string in inches (most common usage of string)
+    public size: number, // Size of the string in inches (most common usage of string size)
     public unitWeight: number, // Weight of the string in kg
     public type: string
-  ) {
-    this.name = name;
-    this.size = size;
-    this.unitWeight = unitWeight;
-    this.type = type;
-  }
+  ) {}
 
   getSizeInMeter(): number {
     return this.size / 39.37;
